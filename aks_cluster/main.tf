801ccc6aab6449a742d5ca9f06bbb08d20c3f584
ramandeep mattu <ramandeep.mattu@gmail.com>
2022-11-16 12:59:11 -0800

    Create main.tf

---
+++ aks_cluster/main.tf
@@ -0,0 +1,30 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.cluster_name
+  #kubernetes_version  = var.kubernetes_version
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = var.cluster_name
+
+  default_node_pool {
+    name                = "system"
+    node_count          = 1
+    vm_size             = "Standard_d2pds_v5"
+    type                = "VirtualMachineScaleSets"
+    enable_auto_scaling = false
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    load_balancer_sku = "standard"
+    network_plugin    = "kubenet" # CNI
+  }
+}
+
